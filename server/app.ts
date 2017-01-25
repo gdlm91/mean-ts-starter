857a6e19f822376d16e8ce7500b25db35c925fba
Gerardo Leal <gerard.leal91@gmail.com>
2017-01-25 18:03:50 -0300

    Implemented Morgan as Logger

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import * as path from 'path';
 import * as bodyParser from 'body-parser';
+import * as logger from 'morgan';
 
 import { initRestApi } from './api'
 
@@ -8,6 +9,7 @@ const app: express.Application = express();
 
 app.disable('x-powered-by');
 
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
